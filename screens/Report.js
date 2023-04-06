dcd436d3c27609e7cf730957758f31a468d0c3b8
thangdoggy <hhqthang@gmail.com>
2023-04-06 19:36:57 +0700

    fix forecast loading

--- screens/Report.js
+++ screens/Report.js
@@ -20,145 +20,138 @@ const Report = () => {
 
   const { data, isLoading, error } = forecastDay(pin.lon, pin.lat);
 
-  return isLoading ? (
-    <>
-      <ActivityIndicator
-        size="large"
-        color="white"
-        style={{ paddingTop: '20%' }}
-      />
-      <Text
-        style={{
-          color: 'white',
-          textAlign: 'center',
-          paddingTop: 10,
-          paddingBottom: '20%',
-          fontSize: SIZES.large,
-        }}
-      >
-        Loading...
-      </Text>
-    </>
-  ) : error ? (
-    <Text
-      style={{
-        color: 'white',
-        textAlign: 'center',
-        paddingTop: 10,
-        paddingBottom: '20%',
-        fontSize: SIZES.large,
-      }}
-    >
-      Oops, something went wrong!
-    </Text>
-  ) : (
+  return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.primary }}>
       <ScrollView style={{ flex: 1 }} showsVerticalScrollIndicator="false">
-        <View style={{ zIndex: 1 }}>
-          <HomeHeader location={location} />
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              alignItems: 'flex-end',
-              margin: SIZES.large,
-            }}
-          >
+        {isLoading ? (
+          <>
+            <ActivityIndicator
+              size="large"
+              color="white"
+              style={{ paddingTop: '20%' }}
+            />
             <Text
               style={{
-                fontFamily: FONTS.bold,
-                fontSize: 28,
-                color: COLORS.white,
+                color: 'white',
+                textAlign: 'center',
+                paddingTop: 10,
+                paddingBottom: '20%',
+                fontSize: SIZES.large,
               }}
             >
-              7 day Forecast
+              Loading...
             </Text>
-          </View>
-          <View
+          </>
+        ) : error ? (
+          <Text
             style={{
-              borderWidth: 1,
-              borderRadius: 15,
-              paddingVertical: 5,
-              backgroundColor: 'rgba(211,211,211,0.3)',
-              width: '95%',
-              marginLeft: 10,
+              color: 'white',
+              textAlign: 'center',
+              paddingTop: 10,
+              paddingBottom: '20%',
+              fontSize: SIZES.large,
             }}
           >
-            <ForecastLine
-              day={data[0].day}
-              aqi={10}
-              icon={data[0].icon}
-              temp1={data[0].temperature_min}
-              temp2={data[0].temperature_max}
-              wind={data[0].wind.speed}
-              angle={data[0].wind.angle}
-            />
-            <ForecastLine
-              day={data[1].day}
-              aqi={70}
-              icon={data[1].icon}
-              temp1={data[1].temperature_min}
-              temp2={data[1].temperature_max}
-              wind={data[1].wind.speed}
-              angle={data[1].wind.angle}
-            />
-            <ForecastLine
-              day={data[2].day}
-              aqi={185}
-              icon={data[2].icon}
-              temp1={data[2].temperature_min}
-              temp2={data[2].temperature_max}
-              wind={data[2].wind.speed}
-              angle={data[2].wind.angle}
-            />
-            <ForecastLine
-              day={data[3].day}
-              aqi={90}
-              icon={data[3].icon}
-              temp1={data[3].temperature_min}
-              temp2={data[3].temperature_max}
-              wind={data[3].wind.speed}
-              angle={data[3].wind.angle}
-            />
-            <ForecastLine
-              day={data[4].day}
-              aqi={300}
-              icon={data[4].icon}
-              temp1={data[4].temperature_min}
-              temp2={data[4].temperature_max}
-              wind={data[4].wind.speed}
-              angle={data[4].wind.angle}
-            />
-            <ForecastLine
-              day={data[5].day}
-              aqi={255}
-              icon={data[5].icon}
-              temp1={data[5].temperature_min}
-              temp2={data[5].temperature_max}
-              wind={data[5].wind.speed}
-              angle={data[5].wind.angle}
-            />
-            <ForecastLine
-              day={data[6].day}
-              aqi={1000}
-              icon={data[6].icon}
-              temp1={data[6].temperature_min}
-              temp2={data[6].temperature_max}
-              wind={data[6].wind.speed}
-              angle={data[6].wind.angle}
-            />
+            Oops, something went wrong!
+          </Text>
+        ) : (
+          <View style={{ zIndex: 1 }}>
+            <HomeHeader location={location} />
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'flex-end',
+                margin: SIZES.large,
+              }}
+            >
+              <Text
+                style={{
+                  fontFamily: FONTS.bold,
+                  fontSize: 28,
+                  color: COLORS.white,
+                }}
+              >
+                7 day Forecast
+              </Text>
+            </View>
+            <View
+              style={{
+                borderWidth: 1,
+                borderRadius: 15,
+                paddingVertical: 5,
+                backgroundColor: 'rgba(211,211,211,0.3)',
+                width: '95%',
+                marginLeft: 10,
+              }}
+            >
+              <ForecastLine
+                day={data[0].day}
+                aqi={10}
+                icon={data[0].icon}
+                temp1={data[0].temperature_min}
+                temp2={data[0].temperature_max}
+                wind={data[0].wind.speed}
+                angle={data[0].wind.angle}
+              />
+              <ForecastLine
+                day={data[1].day}
+                aqi={70}
+                icon={data[1].icon}
+                temp1={data[1].temperature_min}
+                temp2={data[1].temperature_max}
+                wind={data[1].wind.speed}
+                angle={data[1].wind.angle}
+              />
+              <ForecastLine
+                day={data[2].day}
+                aqi={185}
+                icon={data[2].icon}
+                temp1={data[2].temperature_min}
+                temp2={data[2].temperature_max}
+                wind={data[2].wind.speed}
+                angle={data[2].wind.angle}
+              />
+              <ForecastLine
+                day={data[3].day}
+                aqi={90}
+                icon={data[3].icon}
+                temp1={data[3].temperature_min}
+                temp2={data[3].temperature_max}
+                wind={data[3].wind.speed}
+                angle={data[3].wind.angle}
+              />
+              <ForecastLine
+                day={data[4].day}
+                aqi={300}
+                icon={data[4].icon}
+                temp1={data[4].temperature_min}
+                temp2={data[4].temperature_max}
+                wind={data[4].wind.speed}
+                angle={data[4].wind.angle}
+              />
+              <ForecastLine
+                day={data[5].day}
+                aqi={255}
+                icon={data[5].icon}
+                temp1={data[5].temperature_min}
+                temp2={data[5].temperature_max}
+                wind={data[5].wind.speed}
+                angle={data[5].wind.angle}
+              />
+              <ForecastLine
+                day={data[6].day}
+                aqi={1000}
+                icon={data[6].icon}
+                temp1={data[6].temperature_min}
+                temp2={data[6].temperature_max}
+                wind={data[6].wind.speed}
+                angle={data[6].wind.angle}
+              />
+            </View>
           </View>
-          {/* <DayChart /> */}
-        </View>
+        )}
       </ScrollView>
-      <View>
-        <Image
-          style={{
-            width: 50,
-            height: 50,
-          }}
-        />
-      </View>
     </SafeAreaView>
   );
 };
