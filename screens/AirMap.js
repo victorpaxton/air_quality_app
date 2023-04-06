2b4b6459b1c618eb4ebabb16292c13b6a63c01d2
Nguyen Lam Tung <tung.nguyen.mauxox@gmail.com>
2023-04-06 23:17:50 +0700

    New Map in AirMap

--- screens/AirMap.js
+++ screens/AirMap.js
@@ -1,24 +1,202 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useState, useEffect, useRef } from "react";
+import MapView, { LatLng, Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import {
+  StyleSheet,
+  View,
+  Dimensions,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import * as Location from "expo-location";
+import {
+  GooglePlaceDetail,
+  GooglePlacesAutocomplete,
+} from "react-native-google-places-autocomplete";
+import Constants from "expo-constants";
+import MapViewDirections from "react-native-maps-directions";
 
-import MapView from 'react-native-maps';
+const GOOGLE_API_KEY = "AIzaSyDsND-bXr0N929TOcVDGjFPrckd3cc0KBU";
 
-const AirMap = () => {
+const { width, height } = Dimensions.get("window");
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.02;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const INITIAL_POSITION = {
+  // ĐH Bách Khoa TP HCM
+  latitude: 10.77217249292377,
+  longitude: 106.6578910710957,
+  latitudeDelta: LATITUDE_DELTA,
+  longitudeDelta: LONGITUDE_DELTA,
+};
+
+function InputAutocomplete({ label, placeholder = "", onPlaceSelected }) {
+  return (
+    <>
+      <Text>{label}</Text>
+      <GooglePlacesAutocomplete
+        styles={{ textInput: styles.input }}
+        placeholder={placeholder}
+        fetchDetails
+        onPress={(data, details) => {
+          onPlaceSelected(details);
+        }}
+        query={{
+          key: GOOGLE_API_KEY,
+          language: "pt-BR",
+        }}
+      />
+    </>
+  );
+}
+
+export default function AirMap() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  const [showDiretions, setShowDirections] = useState(false);
+
+  const mapRef = useRef(null);
+
+  const moveTo = async (position) => {
+    const camera = await mapRef.current?.getCamera();
+    if (camera) {
+      camera.center = position;
+      mapRef.current?.animateCamera(camera, { duration: 1000 });
+    }
+    set(position);
+    moveTo(position);
+  };
+
+  const [origin, setOrigin] = useState(null);
+  const [destination, setDestination] = useState(null);
+
+  const edgePaddingValue = 10;
+  const edgePadding = {
+    top: edgePaddingValue,
+    right: edgePaddingValue,
+    bottom: edgePaddingValue,
+    left: edgePaddingValue,
+  };
+
+  const traceRoute = () => {
+    if(origin && destination) {
+      setShowDirections(true);
+      mapRef.current?.fitToCoordinates([origin, destination], {edgePadding});
+    }
+  }
+
+  const onPlaceSelected = (details, flag) => {
+    const set = flag === "origin" ? setOrigin : setDestination;
+    const position = {
+      latitude: details.geometry.location.lat || 0,
+      longitude: details.geometry.location.lng || 0,
+    };
+    set(position);
+    moveTo(position);
+  };
+
+  useEffect(() => {
+    const fetchUserLocation = async () => {
+      try {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== "granted") {
+          setErrorMsg("Permission to access location was denied");
+          return;
+        }
+
+        const { coords } = await Location.getCurrentPositionAsync({});
+        setLocation(coords);
+      } catch (error) {
+        console.log("Error while getting user location: ", error);
+        setErrorMsg("Something went wrong while fetching location.");
+      }
+    };
+
+    fetchUserLocation();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView
+        ref={mapRef}
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={INITIAL_POSITION}
+      >
+        {origin && <Marker coordinate={origin} />}
+        {destination && <Marker coordinate={destination} />}
+        {showDiretions && origin && destination && <MapViewDirections
+          origin={origin}
+          destination={destination}
+          apikey={GOOGLE_API_KEY}
+          strokeColor="#6644ff"
+          strokeWidth={4}
+        />}
+      </MapView>
+      <View style={styles.searchContainer}>
+        <InputAutocomplete
+          label="Origin:"
+          placeholder="Enter origin"
+          onPlaceSelected={(details) => {
+            onPlaceSelected(details, "origin");
+          }}
+        />
+        <InputAutocomplete
+          label="Destination:"
+          placeholder="Enter destination"
+          onPlaceSelected={(details) => {
+            onPlaceSelected(details, "destination");
+          }}
+        />
+        <TouchableOpacity 
+          style={styles.button}
+          onPress={traceRoute}
+        >
+          <Text style={styles.buttonText}>Trace Route</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   map: {
-    width: '100%',
-    height: '100%',
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
+  searchContainer: {
+    position: "absolute",
+    width: "90%",
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    elevation: 4,
+    padding: 6,
+    borderRadius: 8,
+    top: Constants.statusBarHeight,
+  },
+  input: {
+    borderColor: "#888",
+    borderWidth: 1,
+  },
+  button: {
+    backgroundColor: "#bbb",
+    paddingVertical: 6,
+    marginTop: 5,
+    borderRadius: 8,
+    width: "30%",
+    position: "relative",
+    left: "35%",
+  },
+  buttonText: {
+    textAlign: "center",
+  }
 });
-
-export default AirMap;
